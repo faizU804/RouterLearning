9692ad379ba5c918675f920f91c594392170f4c8
faiz_804 <faizusardar900@gmail.com>
2025-08-14 01:43:17 +0500

    first commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+   const [data , setData] = useState([])
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/hiteshchoudhary')
+        .then(response => response.json())
+        .then(data => setData(data)
+        )
+    },[])
+  return (
+    <div className='flex justify-center items-center flex-col'>
+    <div className='text-center text-white m-4 bg-gray-600 p-6 rounded duration-500 text-3xl'> Github followers : {data.followers}
+     </div>
+     <div >
+          <img className="rounded-[50%] cursor-pointer" src={data.avatar_url} alt="" />
+     </div>
+     </div>
+  )
+}
+
+export default Github
